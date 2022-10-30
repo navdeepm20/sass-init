7cdb5cde486ce4c6d6c9b2d651f92a62ffcb9fc3
Navdeep <navdeep@neurobit.com>
2022-10-31 01:07:30 +0530

    added custoize support

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 var concat = require("gulp-concat");
-
+const purgecss = require("gulp-purgecss");
 function buildStyles() {
-  return src("./Sass/Zui/**/*.scss")
+  return src("./Sass/sass/**/*.scss")
     .pipe(sass())
+    .pipe(purgecss({ content: ["./Sass/*.html"] }))
     .pipe(concat("style.scss"))
     .pipe(sass().on("error", sass.logError))
     .pipe(dest("./Sass/css/"));
 }
 
 function watchTask() {
-  watch(["./Sass/Zui/**/*.scss"], buildStyles);
+  watch(["./Sass/sass/**/*.scss", "./Sass/*.html"], buildStyles);
 }
 exports.default = series(buildStyles, watchTask);
